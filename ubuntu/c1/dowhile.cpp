71c0a0fd16980015b0eccb6760c60f09b48c8cbb
Islam <islamkwnd@gmail.com>
2022-03-08 15:09:46 +0100

    Init config, should work universally. Awaiting include dirs

---
+++ ubuntu/c1/dowhile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x = 0;
+  do {
+    x++;
+    std::cout << x << std::endl;
+    return 0;
+  } while (x <= 10);
+}
---
+++ ubuntu/c1/in.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+// lmao , doesn't allow void main fxs
+int main(){
+
+	int x;
+	// This works just fine
+	cin >>  x;
+	// Interesting endl
+	cout << x << endl;
+	return 0;
+
+}
