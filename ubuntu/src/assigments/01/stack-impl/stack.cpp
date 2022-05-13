c8db919e656557d6c3ac4e16303143151c7718f7
Islam <islamkwnd@gmail.com>
2022-05-13 18:22:36 +0100

    stack finally complete

--- ubuntu/src/assigments/01/stack-impl/stack.cpp
+++ ubuntu/src/assigments/01/stack-impl/stack.cpp
@@ -74,21 +74,6 @@ void Stack::display() {
               << " = " << elements[i] << '\n';
   }
 }
-//   Unnatural ---
-//   change() -- Replace el at position
-int Stack::change(int element, int position) {
-  // 1. Underflow
-  if (isEmpty()) return 1;
-
-  // 2. Overflow
-  if (position > top) return 2;
-  //   Invariant for swap: top > position >= 0;
-  if (position < 0) return 3;
-
-  // Else swap at pos.
-  elements[position] = element;
-  return 0;
-}
 
 // #[test]
 // Stack test:
@@ -102,7 +87,7 @@ int menuTest() {
   // Get choice and run stack methods based on it
   int choice;
   Stack s;
-  while (choice != 8) {
+  while (choice != 7) {
     choice = getChoice();
     switch (choice) {
       case 1: {
@@ -117,7 +102,12 @@ int menuTest() {
         break;
       }
       case 2:
-        cout << "The topmost element is: " << s.peek() << endl;
+        try {
+          int t = s.peek();
+          cout << "The topmost element is: " << t << endl;
+        } catch (std::logic_error& e) {
+          cout << "Exception: " << e.what() << endl;
+        }
         break;
       case 3:
         try {
@@ -128,48 +118,29 @@ int menuTest() {
           std::cerr << "Exception: " << e.what() << '\n';
         }
         break;
-      case 4: {
-        cout << "Enter the element to change with on the stack" << endl;
-        int element2;
-        cin >> element2;
-        cout << "Enter the position to change with on the stack" << endl;
-        int position;
-        cin >> position;
-        int stackChangeExitCode = s.change(element2, position);
-        if (stackChangeExitCode == 0) {
-          cout << "Element changed" << endl;
-        } else if (stackChangeExitCode == 1) {
-          cout << "Error: Underflow" << endl;
-        } else if (stackChangeExitCode == 2) {
-          cout << "Error: Overflow" << endl;
-        } else if (stackChangeExitCode == 3) {
-          cout << "Error: Invalid position" << endl;
-        }
+      case 4:
         s.display();
         break;
-      }
       case 5:
-        s.display();
-        break;
-      case 6:
         if (s.isEmpty()) {
           cout << "Stack is empty" << endl;
         } else {
           cout << "Stack is not empty" << endl;
         }
         break;
-      case 7:
+      case 6:
         if (s.isFull()) {
           cout << "Stack is full" << endl;
         } else {
           cout << "Stack is not full" << endl;
         }
         break;
-      case 8:
+      case 7:
         cout << "Exiting..." << endl;
         break;
     }
   }
+  return 0;
 }
 int getChoice() {
   // Switch-case menu for each stack method
@@ -177,11 +148,10 @@ int getChoice() {
   cout << "1. Push" << endl;
   cout << "2. Peek" << endl;
   cout << "3. Pop" << endl;
-  cout << "4. Change" << endl;
-  cout << "5. Display" << endl;
-  cout << "6. isEmpty" << endl;
-  cout << "7. isFull" << endl;
-  cout << "8. Exit" << endl;
+  cout << "4. Display" << endl;
+  cout << "5. isEmpty" << endl;
+  cout << "6. isFull" << endl;
+  cout << "7. Exit" << endl;
   cout << "Enter your choice: ";
   cin >> choice;
   return choice;
@@ -237,16 +207,6 @@ int test() {
   if (!st1.isEmpty()) {
     if (popWorks) cout << "Pop elements are being returned as expected";
     throw std::logic_error("pop is broken");
-  }
-  // change
-  st1.push(1);
-  st1.push(2);
-  int n = st1.change(-1, 0);
-  st1.pop();
-  signed int minusOne = st1.pop();
-  bool changeWorks = (minusOne == -1) && (n == 0);
-  if (!changeWorks) {
-    throw std::logic_error("Change is broken");
   }
   return 0;
 }
--- ubuntu/src/assigments/01/stack-impl/stack_submitted.cpp
+++ ubuntu/src/assigments/01/stack-impl/stack_submitted.cpp
@@ -93,7 +93,6 @@ int Stack::change(int element, int position) {
 // #[test]
 // Stack test:
 using namespace std;
-int test();
 int getChoice();
 int menuTest();
 int main() { return menuTest(); }
@@ -102,7 +101,7 @@ int menuTest() {
   // Get choice and run stack methods based on it
   int choice;
   Stack s;
-  while (choice != 8) {
+  while (choice != 7) {
     choice = getChoice();
     switch (choice) {
       case 1: {
@@ -117,7 +116,12 @@ int menuTest() {
         break;
       }
       case 2:
-        cout << "The topmost element is: " << s.peek() << endl;
+        try {
+          int t = s.peek();
+          cout << "The topmost element is: " << t << endl;
+        } catch (std::logic_error& e) {
+          cout << "Exception: " << e.what() << endl;
+        }
         break;
       case 3:
         try {
@@ -128,48 +132,29 @@ int menuTest() {
           std::cerr << "Exception: " << e.what() << '\n';
         }
         break;
-      case 4: {
-        cout << "Enter the element to change with on the stack" << endl;
-        int element2;
-        cin >> element2;
-        cout << "Enter the position to change with on the stack" << endl;
-        int position;
-        cin >> position;
-        int stackChangeExitCode = s.change(element2, position);
-        if (stackChangeExitCode == 0) {
-          cout << "Element changed" << endl;
-        } else if (stackChangeExitCode == 1) {
-          cout << "Error: Underflow" << endl;
-        } else if (stackChangeExitCode == 2) {
-          cout << "Error: Overflow" << endl;
-        } else if (stackChangeExitCode == 3) {
-          cout << "Error: Invalid position" << endl;
-        }
+      case 4:
         s.display();
         break;
-      }
       case 5:
-        s.display();
-        break;
-      case 6:
         if (s.isEmpty()) {
           cout << "Stack is empty" << endl;
         } else {
           cout << "Stack is not empty" << endl;
         }
         break;
-      case 7:
+      case 6:
         if (s.isFull()) {
           cout << "Stack is full" << endl;
         } else {
           cout << "Stack is not full" << endl;
         }
         break;
-      case 8:
+      case 7:
         cout << "Exiting..." << endl;
         break;
     }
   }
+  return 0;
 }
 int getChoice() {
   // Switch-case menu for each stack method
@@ -177,9 +162,10 @@ int getChoice() {
   cout << "1. Push" << endl;
   cout << "2. Peek" << endl;
   cout << "3. Pop" << endl;
-  cout << "4. Change" << endl;
-  cout << "5. Display" << endl;
-  cout << "6. Exit" << endl;
+  cout << "4. Display" << endl;
+  cout << "5. isEmpty" << endl;
+  cout << "6. isFull" << endl;
+  cout << "7. Exit" << endl;
   cout << "Enter your choice: ";
   cin >> choice;
   return choice;
