b98be47d492033437482b1ac1c12a5d8c96d2c15
Islam <islamkwnd@gmail.com>
2022-06-22 22:36:09 +0100

    Started single linked list impl

--- ubuntu/src/fds/list/Single.cpp
+++ ubuntu/src/fds/list/Single.cpp
@@ -1,2 +1,50 @@
+#include <iostream>
+
 #include "islgr_util.h"
-int main() { EXPECT_T(1 == 2); }
+#include "node.h"
+class SinglyLinked {
+ private:
+  Node* head;
+
+ public:
+  SinglyLinked();
+  ~SinglyLinked();
+  //   necessary utils
+  void display();
+  bool isEmpty();
+};
+
+// Constructor. Create an empty SinglyLinked.
+SinglyLinked::SinglyLinked() { head = nullptr; }
+// Empty
+bool SinglyLinked::isEmpty() { return head == nullptr; }
+// Display.
+void SinglyLinked::display() {
+  // Empty.
+  if (isEmpty()) {
+    std::cout << "List is empty" << std::endl;
+    return;
+  }
+  // Traverse and display
+  Node* tmp = head;
+  while (tmp != nullptr) {
+    // tags
+    if (tmp == head) {
+      std::cout << "Head: ";
+    }
+    // display
+    tmp->display();
+    std::cout << std::endl;
+  }
+}
+void tests();
+int main() { tests(); }
+
+void tests() {
+  SinglyLinked* l = new SinglyLinked();
+  // Should be empty, and output empty
+  bool empy = l->isEmpty();
+  EXPECT_T(empy);
+  std::cout << "Empty log" << std::endl;
+  l->display();
+}
